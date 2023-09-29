975d92fc6ee4e4204da242b3e1568413a3876e5a
Dipakkkk <dr916139@gmail.com>
2023-09-29 19:14:08 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// const a = document.getElementById("nav")
+const b = document.getElementById("change");
+function navresponsive() {
+  // a.classList.toggle("show")
+  document.body.classList.toggle("overflow_hidden");
+  b.classList.toggle("change");
+}
