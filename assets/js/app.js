2bdfae6ad12ad94fd5448e74a5cfac87c5f66835
Dipakkkk <dr916139@gmail.com>
2023-09-29 21:49:30 +0530

    second-commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// nav-bar//
+
 // const a = document.getElementById("nav")
 const b = document.getElementById("change");
 function navresponsive() {
@@ -5,3 +7,25 @@ function navresponsive() {
   document.body.classList.toggle("overflow_hidden");
   b.classList.toggle("change");
 }
+
+
+// back-to-top//
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
+
+// preloader//
+//  setTimeout(function () {
+//    document.querySelector(".preloader").classList.add("loader");
+//  }, 3000); 
