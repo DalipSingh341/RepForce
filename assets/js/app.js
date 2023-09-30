8db4de2839ca9b199d5c6b6fbf51fcb4cca1ff07
Dipakkkk <dr916139@gmail.com>
2023-09-30 13:51:25 +0530

    navbar edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 // nav-bar//
 
-// const a = document.getElementById("nav")
+const a = document.getElementById("nav")
 const b = document.getElementById("change");
+// const c = document.classList.add("overflow_visible")
 function navresponsive() {
-  // a.classList.toggle("show")
+  a.classList.toggle("show1")
   document.body.classList.toggle("overflow_hidden");
   b.classList.toggle("change");
 }
